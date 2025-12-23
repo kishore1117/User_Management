299efafe747a04a767b6bd80a2296ced4e267093
Kishore <kishoreranganathan60@gmail.com>
2025-12-23 21:45:13 +0530

    db table command

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import compression from "compression";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import { initDB, pool } from "./src/config/db.js";
 
 // ------------------------
 // 📁 Local Imports
@@ -21,6 +22,9 @@ import db from "./src/config/db.js";
 // ------------------------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+//Initialize Database table creation
+initDB();
 
 // ------------------------
 // 🌱 Environment Variables
